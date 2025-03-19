8e57360ebdc8df4966e6153276a053e13a5edbbe
MaksimLutlouski <114978187+MaksimLutlouski@users.noreply.github.com>
2025-03-19 12:50:18 +0100

    Update CrmApiApplication.java/Spring boot et package

--- crm-api/src/main/java/com/example/crmapi/CrmApiApplication.java
+++ crm-api/src/main/java/com/example/crmapi/CrmApiApplication.java
@@ -1,9 +1,9 @@
-package com.example.crm_api;
+package com.example.crmapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.crm_api")
+@SpringBootApplication(scanBasePackages = "com.example.crmapi")
 public class CrmApiApplication {
 
 	public static void main(String[] args) {
