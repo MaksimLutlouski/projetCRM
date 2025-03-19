aad594ad448374a4b53866106ba42c661c98812e
MaksimLutlouski <114978187+MaksimLutlouski@users.noreply.github.com>
2025-03-19 11:54:35 +0100

    Update CrmApiApplicationTests.java

--- crm-api/src/test/java/com/example/crm_api/CrmApiApplicationTests.java
+++ crm-api/src/test/java/com/example/crm_api/CrmApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.crm_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.example.crm_api.CrmApiApplication.class)
 class CrmApiApplicationTests {
 
 	@Test
